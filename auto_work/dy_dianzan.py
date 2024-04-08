a0c6139259d59cd23782b4629dd821312e21b7c4
zfw <zfw15201543520@gmail.com>
2024-04-08 12:54:06 +0800

    update dianzan autowork

--- auto_work/dy_dianzan.py
+++ auto_work/dy_dianzan.py
@@ -1,24 +1,25 @@
 import time
 
 import uiautomator2 as u2
+
 # https://www.youtube.com/watch?v=ZA6NOhFRaxw
 
 d = u2.connect(addr='fa5lifgiba6tbaug')
+# 打开抖音APP
 
 d(text='抖音').click()
-
+# 点击搜索框
 d(resourceId="com.ss.android.ugc.aweme:id/j=c").click()
 d.send_keys(text='蜘蛛膜')
-
+# 搜索
 d(resourceId="com.ss.android.ugc.aweme:id/yza").click()
 
 # 进入视频
-d.xpath('//*[@resource-id="com.ss.android.ugc.aweme:id/btz"]/android.widget.FrameLayout[1]/com.lynx.tasm.behavior.ui.view.UIView[3]').click()
+d(text="视频，按钮").click()
 # 点赞
 d(className="android.widget.ImageView").click()
 d.swipe_ext("up")
 #
-# d(resourceId="com.ss.android.ugc.aweme:id/container").click()
 time.sleep(1)
 
 while True:
