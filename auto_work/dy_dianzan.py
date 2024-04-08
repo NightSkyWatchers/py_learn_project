a4147a4a483abe829a13691a6208b8585ca27800
zfw <zfw15201543520@gmail.com>
2024-04-08 12:48:02 +0800

    update douyin dianzan autowork

--- auto_work/dy_dianzan.py
+++ auto_work/dy_dianzan.py
@@ -1,23 +1,27 @@
+import time
+
 import uiautomator2 as u2
 # https://www.youtube.com/watch?v=ZA6NOhFRaxw
 
-d = u2.connect()
-
-print(d.info)
+d = u2.connect(addr='fa5lifgiba6tbaug')
 
 d(text='抖音').click()
 
-d(resourceId="com.ss.android.ugc.aweme:id/g=b").click()
+d(resourceId="com.ss.android.ugc.aweme:id/j=c").click()
 d.send_keys(text='蜘蛛膜')
 
-d(resourceId="com.ss.android.ugc.aweme:id/op0").click()
+d(resourceId="com.ss.android.ugc.aweme:id/yza").click()
 
-d(classname="com.lynx.tasm.behavior.ui.view.UIView").click()
-d.swipe_ext("up", scale=0.5)
-
-d(resourceId="com.ss.android.ugc.aweme:id/container").click()
-
+# 进入视频
+d.xpath('//*[@resource-id="com.ss.android.ugc.aweme:id/btz"]/android.widget.FrameLayout[1]/com.lynx.tasm.behavior.ui.view.UIView[3]').click()
+# 点赞
+d(className="android.widget.ImageView").click()
+d.swipe_ext("up")
+#
+# d(resourceId="com.ss.android.ugc.aweme:id/container").click()
+time.sleep(1)
 
 while True:
-    d(resourceId="com.ss.android.ugc.aweme:id/djf").click()
-    d.swipe_ext("up", scale=0.5)
+    d(className="android.widget.ImageView").click()
+    d.swipe_ext("up")
+    time.sleep(1)
